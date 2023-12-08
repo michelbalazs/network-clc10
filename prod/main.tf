dbda52ebf00658ebf1e71712949c1af40598cb13
Michel Balazs <michel.balazs@gmail.com>
2023-12-07 23:02:28 -0300

    update

---
+++ prod/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    
+    default_tags {
+        tags = {
+            Owner   = "Vinicius Cavalcanti"
+            Env     = "Producao New Teste"
+            Class   = "CLC10"
+        }
+    }
+  # Configuration options
+}
